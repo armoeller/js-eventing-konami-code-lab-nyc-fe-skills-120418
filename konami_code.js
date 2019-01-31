1972bd9f7882f7ad6185013d0801ba0252c25054
Andy Moeller <ar.moeller1@gmail.com>
2019-01-31 23:09:26 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let tracker = 0
 
-//2 - make the function to trigger
+// 2 - make the function to trigger
 const checkKeyCode = (event) => {
-  //we need to know where we are in the sequence
+  // we need to know where we are in the sequence
   const whatTheyPressed = event.keyCode
-  if (whatTheyPressed === code[tracker]){
+  if (whatTheyPressed === code[tracker]) {
     tracker += 1
   } else {
     tracker = 0
@@ -15,8 +15,8 @@ const checkKeyCode = (event) => {
 
 function init() {
   // Write your JavaScript code inside the init() function
-  //keydown
-  // 1 - find an elemnt? nope we're just need the document.
+  // keydown
+  // 1 - find an element? nope - we're just gonna need the document
   
   document.addEventListener('keydown', checkKeyCode)
   }
